bcbcd75320b1742b8fac8528eb516683d9e489e1
Rodrigo Santiago Gomide <rodrigogomide.def@gmail.com>
2022-07-29 17:48:35 -0300

    R3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,12 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  const funcionarios = data.employees
+  if (!employeeName) {
+    return {}
+  }
+  return funcionarios.find(element => element.firstName == employeeName || element.lastName == employeeName)
 }
-
+console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
