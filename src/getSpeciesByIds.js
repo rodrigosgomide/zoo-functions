74b870a8007c87d19fb0243d5abc15ba4c1d5dce
Rodrigo Santiago Gomide <rodrigogomide.def@gmail.com>
2022-07-29 15:33:19 -0300

    R1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,8 +1,13 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  
-  // seu código aqui
+function getSpeciesByIds(... ids) {
+  const animais = []
+  ids.forEach((id) => {
+    const animal = species.find((element) => element.id == id)
+    animais.push(animal)
+  }) 
+  return animais; 
 }
 
 module.exports = getSpeciesByIds;
