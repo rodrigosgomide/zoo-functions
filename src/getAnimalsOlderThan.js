4f81d9eab6d91b3e61c648cfbe3523bf8da237fb
Rodrigo Santiago Gomide <rodrigogomide.def@gmail.com>
2022-07-29 17:59:18 -0300

    R2 - refactor

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -2,8 +2,8 @@ const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getAnimalsOlderThan(animal, age) {
-  const getAnimalByName = species.find(element => element.name == animal)
-  return getAnimalByName.residents.map((element) => element.age).every((element) => element > age)
+  const animals = species.find(element => element.name == animal).residents
+  return animals.every((element) => element.age > age)
 }
 // console.log(getAnimalsOlderThan('lions', 1));
 
