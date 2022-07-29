13c82dd567d92f460382c24232bc898edce4d73d
Rodrigo Santiago Gomide <rodrigogomide.def@gmail.com>
2022-07-29 19:41:56 -0300

    R4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,15 @@
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  return data.employees.some((empregado) => empregado.managers.includes(id))
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  return data.employees.filter((empregado) => empregado.managers.includes(managerId))
+    .map((empregado) => `${empregado.firstName} ${empregado.lastName}`)
 }
 
 module.exports = { isManager, getRelatedEmployees };
