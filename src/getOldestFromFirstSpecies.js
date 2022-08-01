5d778b71c60fdd80adc0bbcace45eb51c2f28b47
Rodrigo Santiago Gomide <rodrigogomide.def@gmail.com>
2022-08-01 19:13:58 -0300

    R10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,11 @@
+const { species, employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const animalId = employees.find((funcionario) => funcionario.id === id).responsibleFor[0];
+  const animals = species.find((animal) => animal.id === animalId).residents;
+  const oldestAnimal = animals.reduce((animal, currenAnimal) => animal.age > currenAnimal.age ? animal : currenAnimal)
+  return Object.values(oldestAnimal)
 }
 
 module.exports = getOldestFromFirstSpecies;
